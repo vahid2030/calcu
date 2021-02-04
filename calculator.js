b4b13a8530329570429ad4aa01ab83ad5be563bb
vahid <vhd_jll@yahoo.com>
2021-02-04 03:44:05 -0600

    second modification

--- calculator.js
+++ calculator.js
@@ -1,34 +1,32 @@
-// Ask the user for first the number.
+// Ask the user for the first number.
 // Ask the user for the second number.
-// Ask the user for and operation to perform.
-// Perform the operation on two number.
-// print the result to the terminal.
-
-// calculator
+// ask the user for an operation they wnat to perform.
+// perform operation on two number.
+// print the result to the terminal;
 
 
 const readline = require('readline-sync');
 
-console.log('Welcome to calculator');
+console.log('welcome to calculator');
 
-console.log('what is first number?');
+console.log('what is your first number?');
 let number1 = readline.question();
 
-console.log('what is second number?');
+console.log('what is your second number');
 let number2 = readline.question();
 
-console.log('what operation would like to perform/n1) Add 2) subtraction 3) Multiply 4) Divide');
+console.log('what operation do you want to perform?\n1) Add 2) Subtract 3) Multiply 4) divide');
 let operation = readline.question();
 
-let output ;
+let ouput;
 if (operation === '1') {
-  output = Number( number1) + Number(number2);
+  output = Number(number1) + Number(number2);
 } else if (operation === '2') {
-  output = Number(number1) - Number(number2);
+  output =  Number(number1) + Number(number2);
 } else if (operation === '3') {
-  output = Number(number1) * Number(number2);
+  output =  Number(number1) * Number(number2);
 } else if (operation === '4') {
-  output = Number(number1) / Number(number2);
+  output =   Number(number1) / Number(number2);
 }
-  
-console.log(`The result is: ${output}`);
+
+console.log(`The result would be ${output}`);
